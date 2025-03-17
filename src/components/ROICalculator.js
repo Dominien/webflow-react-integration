ea53c526faca53f7463fc83f9635848e9e9b19a3
Marco <marcopatzelt7@gmail.com>
2025-03-17 19:21:14 +0100

    Your commit message

--- src/components/ROICalculator.js
+++ src/components/ROICalculator.js
@@ -70,9 +70,10 @@ const SettingsIcon = () => (
 
 const ChartIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M12 20V10"></path>
-    <path d="M18 20V4"></path>
-    <path d="M6 20v-4"></path>
+    <path d="M3 3v18h18"></path>
+    <path d="M18 17V9"></path>
+    <path d="M13 17V5"></path>
+    <path d="M8 17v-3"></path>
   </svg>
 );
 
